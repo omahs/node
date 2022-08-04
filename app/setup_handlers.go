1c4f2c08839a0529c5f6cae5ce1ff72a450aefbe
Tanmay <tanmay@zetachain.com>
2022-08-04 16:26:51 -0400

    add script to test client

--- app/setup_handlers.go
+++ app/setup_handlers.go
@@ -1,5 +1,35 @@
 package app
 
-const releaseVersion = "test-client"
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	m "github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	zetatypes "github.com/zeta-chain/zetacore/x/zetacore/types"
+)
 
-func SetupHandlers(app *App) {}
+const releaseVersion = "0.2.1"
+
+func SetupHandlers(app *App) {
+	app.UpgradeKeeper.SetUpgradeHandler(releaseVersion, func(ctx sdk.Context, plan types.Plan, vm m.VersionMap) (m.VersionMap, error) {
+		app.Logger().Info("Running upgrade handler for " + releaseVersion)
+		for moduleName, module := range app.mm.Modules {
+			vm[moduleName] = module.ConsensusVersion()
+		}
+		vm[zetatypes.ModuleName] = vm[zetatypes.ModuleName] - 1
+		return app.mm.RunMigrations(ctx, app.configurator, vm)
+	})
+
+	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+	if err != nil {
+		panic(err)
+	}
+	if upgradeInfo.Name == releaseVersion && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		storeUpgrades := storetypes.StoreUpgrades{}
+		// Use upgrade store loader for the initial loading of all stores when app starts,
+		// it checks if version == upgradeHeight and applies store upgrades before loading the stores,
+		// so that new stores start with the correct version (the current height of chain),
+		// instead the default which is the latest version that store last committed i.e 0 for new stores.
+		app.SetStoreLoader(types.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	}
+}

8e9bfdcc3a5782fc2fc453e5bd447c242306f793
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-17 15:20:07 -0400

    test

--- standalone-network/upgrade-integration-download.sh
+++ standalone-network/upgrade-integration-download.sh
@@ -33,8 +33,8 @@ chmod +x $DAEMON_HOME/cosmovisor/genesis/bin/zetaclientd
 zetacored init test --chain-id=$CHAINID -o
 echo "race draft rival universe maid cheese steel logic crowd fork comic easy truth drift tomorrow eye buddy head time cash swing swift midnight borrow" | zetacored keys add zeta --recover --keyring-backend=test
 echo "hand inmate canvas head lunar naive increase recycle dog ecology inhale december wide bubble hockey dice worth gravity ketchup feed balance parent secret orchard" | zetacored keys add mario --recover --keyring-backend=test
-zetacored add-genesis-account $(zetacored keys show zeta -a --keyring-backend=test) 500000000000000000000000000000000stake --keyring-backend=test
-zetacored add-genesis-account $(zetacored keys show mario -a --keyring-backend=test)  500000000000000000000000000000000stake --keyring-backend=test
+zetacored add-genesis-account $(zetacored keys show zeta -a --keyring-backend=test) 500000000000000000000000000000000azeta --keyring-backend=test
+zetacored add-genesis-account $(zetacored keys show mario -a --keyring-backend=test)  500000000000000000000000000000000azeta --keyring-backend=test
 
 cat "$DAEMON_HOME"/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="azeta"' > "$DAEMON_HOME"/config/tmp_genesis.json && mv "$DAEMON_HOME"/config/tmp_genesis.json "$DAEMON_HOME"/config/genesis.json
 cat "$DAEMON_HOME"/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="azeta"' > "$DAEMON_HOME"/config/tmp_genesis.json && mv "$DAEMON_HOME"/config/tmp_genesis.json "$DAEMON_HOME"/config/genesis.json
@@ -44,7 +44,7 @@ cat "$DAEMON_HOME"/config/genesis.json | jq '.app_state["evm"]["params"]["evm_de
 cat "$DAEMON_HOME"/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="500000000"' > "$DAEMON_HOME"/config/tmp_genesis.json && mv "$DAEMON_HOME"/config/tmp_genesis.json "$DAEMON_HOME"/config/genesis.json
 cat "$DAEMON_HOME"/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="10s"' > "$DAEMON_HOME"/config/tmp_genesis.json && mv "$DAEMON_HOME"/config/tmp_genesis.json "$DAEMON_HOME"/config/genesis.json
 
-zetacored gentx zeta 1000000000000000000000000stake --chain-id=localnet --keyring-backend=test
+zetacored gentx zeta 1000000000000000000000000azeta --chain-id=$CHAINID --keyring-backend=$KEYRING
 zetacored collect-gentxs
 zetacored validate-genesis
 
@@ -52,27 +52,28 @@ zetacored validate-genesis
 
 
 
-cosmovisor start --home ~/.zetacore/ --p2p.laddr 0.0.0.0:27655  --grpc.address 0.0.0.0:9096 --grpc-web.address 0.0.0.0:9093 --address tcp://0.0.0.0:27659 --rpc.laddr tcp://127.0.0.1:26657 >> zetanode.log 2>&1  &
-
+cosmovisor start >> ~/.zetacored/cosmovisor.log 2>&1  &
 sleep 7
 printf "Raising the governance proposal:\n"
 zetacored tx gov submit-legacy-proposal software-upgrade v1.0.0 \
   --from zeta \
-  --deposit 10000000000000000000stake \
-  --upgrade-height 6 \
+  --deposit 10000000000000000000azeta \
+  --upgrade-height 10 \
   --upgrade-info '{"binaries":{"zetacored-linux/amd64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetacored-ubuntu-22-amd64?checksum=sha256:8106290054b0297a2d00adaeea0257d1e968fcf71ed50885effd1ffb2edc8c64","zetacored-linux/arm64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetacored-ubuntu-22-arm64?checksum=sha256:533b8938e1f1e8b0e48f6dbbaf509e15cda3fc4f81a971f5534e349d23d7d3da","zetaclientd-linux/amd64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetaclientd-ubuntu-22-arm64?checksum=sha256:d188cdd2dbdad4d1e46d96c8abac9e60039f7bbed5c76539f17568329d391232"}}' \
-  --description "test-" \
+  --description "test" \
   --title "test-upgrade" \
   --from zeta \
   --keyring-backend $KEYRING \
   --chain-id $CHAINID \
+  --gas=auto --gas-adjustment=1.5 --gas-prices=0.001azeta \
   --yes
 sleep 7
 zetacored tx gov vote 1 yes --from zeta --keyring-backend $KEYRING --chain-id $CHAINID --yes
 clear
 sleep 10
 zetacored query gov proposal 1
-tail -f zetanode.log
+
+tail -f ~/.zetacored/cosmovisor.log
 
 #'{"binaries":{"zetaclientd-darwin/arm64":"https://filebin.net/4awhitgraq8eenpd/zetaclientd","zetacored-darwin/arm64":"https://filebin.net/4awhitgraq8eenpd/zetacored"}}'
 #'{"binaries":{"zetacored-linux/amd64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetacored-ubuntu-22-amd64?checksum=sha256:8106290054b0297a2d00adaeea0257d1e968fcf71ed50885effd1ffb2edc8c64","zetacored-linux/arm64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetacored-ubuntu-22-arm64?checksum=sha256:533b8938e1f1e8b0e48f6dbbaf509e15cda3fc4f81a971f5534e349d23d7d3da","zetaclientd-linux/amd64": "https://zetachain-external-files.s3.amazonaws.com/binaries/athens3/v1.0.0/zetaclientd-ubuntu-22-arm64?checksum=sha256:d188cdd2dbdad4d1e46d96c8abac9e60039f7bbed5c76539f17568329d391232"}}'
